33e76bdbcb3bf182992abca92b8a56fa94df9b64
azacdev <azacdev@gmail.com>
2023-08-11 19:16:13 +0100

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+import '@styles/global.css';
+
+export const metadata = {
+    title: "Shompts",
+    description: "Discover & Share AI Prompts"
+}
+
+const layout = () => {
+  return <div>layout</div>;
+};
+
+export default layout;
