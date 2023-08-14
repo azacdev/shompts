39bf12ce8e7d53f5bc297467128c5d3a40bf9611
azacdev <azacdev@gmail.com>
2023-08-14 03:10:53 +0100

    implemented create prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -7,12 +7,38 @@ import { useRouter } from "next/navigation";
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
-  const createPrompt = async (e) => {};
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+"use client"
+
+import {useState} from 'react'
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState("")
+
+  const handleSearchChange = (e) => {
+
+  }
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input type="text" placeholder='Search for a tag or a username'
+        value={searchText}
+         onChange={handleSearchChange}
+         required
+         className='search_input peer'/>
+      </form>
+
+      <PrompCardList />
+    </section>
   )
 }
 
