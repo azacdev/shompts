cf5ffbc4084bfe8fe8fae25c1a201bbb6465b238
azacdev <azacdev@gmail.com>
2023-08-15 20:31:03 +0100

    implemented delete functionality

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -26,7 +26,20 @@ const MyProfile = () => {
     router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = async () => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt");
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+        setPosts(filteredPosts);
+      } catch (error) {}
+    }
+  };
 
   return (
     <Profile
@@ -34,7 +47,7 @@ const MyProfile = () => {
       desc="Welcome to your personalined profile page"
       data={posts}
       handleEdit={handleEdit}
-      handleDelete={{}}
+      handleDelete={handleDelete}
     />
   );
 };
