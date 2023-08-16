65fd0fc2b2e6a3b0d9662a8f71abbc2410596367
azacdev <azacdev@gmail.com>
2023-08-16 03:30:23 +0100

    added feed input placeholder

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -56,7 +56,7 @@ const Feed = () => {
       <form className="relative w-full flex-center">
         <input
           type="text"
-          placeholder="Search for a tag or a username"
+          placeholder="Search for prompt or a tag"
           value={searchText}
           onChange={handleSearchChange}
           required
