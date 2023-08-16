d3fdcc9ae19ab00703c5c24cb3adeaa6cdbac09e
azacdev <azacdev@gmail.com>
2023-08-16 03:33:03 +0100

    home page desc update

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <span className="green_lime_gradient">AI-Powered Prompts</span>
       </h1>
       <p className="desc text-center">
-        Shompts is an open-source AI prompting tool for modern world to
+        Shompts is an AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
 
