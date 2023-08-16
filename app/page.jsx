c65e4f5f3a37d2521515cc81f747474f846c75b9
azacdev <azacdev@gmail.com>
2023-08-16 03:26:43 +0100

    ui revamped

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ const Home = () => {
       <h1 className="head_text text-center">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="orange_gradient">AI-Powered Prompts</span>
+        <span className="green_lime_gradient">AI-Powered Prompts</span>
       </h1>
       <p className="desc text-center">
         Shompts is an open-source AI prompting tool for modern world to
--- components/Form.jsx
+++ components/Form.jsx
@@ -4,11 +4,11 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text-left">
-        <span className="blue_gradient">{type} Post</span>
+        <span className="blueish_lime_gradient">{type} Post</span>
       </h1>
       <p className="desc text-left max-w-md">
-        {type} and share amazing propmts with the world, and let your
-        imagination run wild with any AI-powered platform
+        {type} and share incredible prompts with the world and unleash your
+        creativity on any AI-powered platform.
       </p>
 
       <form
