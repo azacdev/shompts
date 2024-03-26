65851b848dff7ff3738a8dce289c4c15cf506458
azacdev <azacdev@gmail.com>
2024-03-26 15:21:01 +0100

    chore: update mongodb uri

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["mongoose"],
-  },
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
   },
   webpack(config) {
     config.experiments = {
